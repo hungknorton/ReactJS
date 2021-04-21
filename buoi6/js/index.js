9cf4f0cfc1eccec6bf59b0bf6a07f7f5ee79ca8a
Tran Ngoc Hung <hungkhmt2k4@gmail.com>
2021-04-21 12:37:57 +0700

    buoi6

---
+++ buoi6/js/index.js
@@ -0,0 +1,16 @@
+console.log('kakakak')
+// Khai bao bien
+// var, let, const
+var a = 19
+var b = 'chuoi'
+var d = `day "la" chuoi 3 ${b}`
+var c = a + b
+console.log(typeof car)
+console.log(d)
+var car = {
+  color: 'red',
+  price: 1000,
+}
+var arr = ['', 1, 4, true]
+console.log(car['color'])
+console.log(car)
